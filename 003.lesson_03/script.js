1a217f90da0e53db240747b87d1ab05b40cc701c
Kolesnikov Denys <ukrainianboyzz@gmail.com>
2023-02-20 20:09:01 +0200

    HW done

---
+++ 003.lesson_03/script.js
@@ -0,0 +1,16 @@
+// first point
+
+let getString1 = prompt('Input first string');
+let getString2 = prompt('Input second string');
+let getString3 = prompt('Input third string');
+
+alert(`${getString1}\n` + `${getString2}\n` + `${getString3}`);
+
+// second point
+
+let getNumber = prompt('Пятизначное число, пожалуйста:');
+alert(getNumber.toString().split('').join(' '));
+
+
+
+
