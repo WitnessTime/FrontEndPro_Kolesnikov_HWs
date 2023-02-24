eada56ee6f118ccddf202a89d6d9d2137e51bb95
Kolesnikov Denys <ukrainianboyzz@gmail.com>
2023-02-24 16:37:55 +0200

    004.lesson_04 done

---
+++ 004.lesson_04/script.js
@@ -0,0 +1,7 @@
+let firstNumber = +prompt("Введіть перше число:");
+let secondNumber = +prompt("Введіть друге число:");
+
+alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber},
+${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber},
+${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}
+${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
